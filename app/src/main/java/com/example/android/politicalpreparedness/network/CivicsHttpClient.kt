bf5ce394b3441355cbebf3a77d36a161d7e5e65a
Fabian Godinez Chavez <fabianc@backbase.com>
2023-12-19 15:11:16 -0600

    first commit

---
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -0,0 +1,29 @@
+package com.example.android.politicalpreparedness.network
+
+import okhttp3.OkHttpClient
+
+class CivicsHttpClient : OkHttpClient() {
+
+    companion object {
+
+        private const val API_KEY = "AIzaSyCoU2Ut89AwdQgI8lefb-lTNZxVSJATGaU"
+
+        fun getClient(): OkHttpClient {
+            return Builder()
+                .addInterceptor { chain ->
+                    val original = chain.request()
+                    val url = original
+                        .url()
+                        .newBuilder()
+                        .addQueryParameter("key", API_KEY)
+                        .build()
+                    val request = original
+                        .newBuilder()
+                        .url(url)
+                        .build()
+                    chain.proceed(request)
+                }
+                .build()
+        }
+    }
+}
